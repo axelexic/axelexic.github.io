b471943d6539068c014b344f55d21730837bd332
Yogesh Swami <axelexic@gmail.com>
2026-05-13 12:25:58 -0700

    Move to github actions
    Move to github actions and better support for markdown single dollar sign rendering.

---
+++ _plugins/standard_tex_math.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "cgi"
+
+module StandardTexMath
+  TOKEN = "JXKMATH%<index>dXKJ"
+
+  def convert(content)
+    masked, math = mask_standard_tex_math(content)
+    html = super(masked)
+    math.each_with_index do |source, index|
+      html = html.gsub(format(TOKEN, index: index), CGI.escapeHTML(source))
+    end
+    html
+  end
+
+  private
+
+  def mask_standard_tex_math(content)
+    math = []
+    masked = content.gsub(/(?<!\\)\$\$(.+?)(?<!\\)\$\$/m) do
+      token = format(TOKEN, index: math.length)
+      math << Regexp.last_match(0)
+      token
+    end
+
+    masked = masked.gsub(/(?<!\\)(?<!\$)\$(?!\$)([^\n]+?)(?<!\\)\$(?!\$)/) do
+      token = format(TOKEN, index: math.length)
+      math << Regexp.last_match(0)
+      token
+    end
+
+    [masked, math]
+  end
+end
+
+Jekyll::Converters::Markdown::KramdownParser.prepend(StandardTexMath)
